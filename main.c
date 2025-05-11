47d71792ff9b5a6d2e65cd909a8f3b13874d694c
Saltspeddy <walkercoco30@gmail.com>
2025-05-11 21:43:55 +0300

    My personal designer(Miri) improved the Chess game's look and added more buttons to offer some future functionality

--- main.c
+++ main.c
@@ -2,14 +2,14 @@
 #include <windows.h>
 #include <SDL2/SDL.h>       // Correct if files are in include/SDL2/ // or #include "SDL.h" depending on setup
 #include <SDL2/SDL_image.h> // Required for PNG/JPG
-// #include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_ttf.h>
 #include <stdio.h>
 #include <string.h>
 
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 800
+#define WINDOW_WIDTH 750
+#define WINDOW_HEIGHT 600
 #define BOARD_SIZE 8
-#define SQUARE_SIZE (500 / BOARD_SIZE)
+#define SQUARE_SIZE (400 / BOARD_SIZE)
 
 enum PieceType
 {
@@ -54,11 +54,6 @@ int validateMove(int initCol, int initRow, int destCol, int destRow)
     {
         return 0;
     }
-
-    // if (whosTurn != Piece.color)
-    // {
-    //     return 0;
-    // }
 
     int difCol = destCol - initCol;
     int difRow = destRow - initRow;
@@ -291,12 +286,99 @@ void render_piece(SDL_Renderer *renderer, SDL_Texture *piece, int col, int row)
     int img_width, img_height;
     SDL_QueryTexture(piece, NULL, NULL, &img_width, &img_height);
     SDL_Rect dstrect = {
-        100 + SQUARE_SIZE * col + ((SQUARE_SIZE - img_width) / 2),  // x position (centered)
-        100 + SQUARE_SIZE * row + ((SQUARE_SIZE - img_height) / 2), // y position (centered)
-        img_width,                                                  // width
-        img_height                                                  // height
+        80 + SQUARE_SIZE * col + ((SQUARE_SIZE - img_width) / 2),  // x position (centered)
+        80 + SQUARE_SIZE * row + ((SQUARE_SIZE - img_height) / 2), // y position (centered)
+        img_width,                                                 // width
+        img_height                                                 // height
     };
     SDL_RenderCopy(renderer, piece, NULL, &dstrect);
+}
+
+void render_multiline_text(SDL_Renderer *renderer, TTF_Font *font, const char *text, SDL_Color color, int x, int y, int line_spacing)
+{
+    char *text_copy = strdup(text); // Duplicate string so we can modify it
+    if (!text_copy)
+        return;
+
+    char *line = strtok(text_copy, "\n");
+    int offset_y = 0;
+
+    while (line)
+    {
+        SDL_Surface *surface = TTF_RenderUTF8_Blended(font, line, color);
+        SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+        SDL_Rect dst = {x, y + offset_y, surface->w, surface->h};
+        SDL_RenderCopy(renderer, texture, NULL, &dst);
+
+        offset_y += surface->h + line_spacing;
+
+        SDL_FreeSurface(surface);
+        SDL_DestroyTexture(texture);
+
+        line = strtok(NULL, "\n");
+    }
+
+    free(text_copy);
+}
+
+void start_game()
+{
+    printf("Start button clicked\n");
+    // TODO: Add game starting logic here
+}
+
+void save_game()
+{
+    printf("Save button clicked\n");
+    // TODO: Add save functionality here
+}
+
+void reload_game()
+{
+    printf("Reload button clicked\n");
+    // TODO: Add reload logic here
+}
+
+void open_progress_window()
+{
+    // SDL_Window *progressWindow = SDL_CreateWindow("Progress",
+    //                                               SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 300, 300, SDL_WINDOW_SHOWN);
+    // if (!progressWindow)
+    // {
+    //     SDL_Log("Failed to create progress window: %s", SDL_GetError());
+    //     return;
+    // }
+
+    // SDL_Renderer *progressRenderer = SDL_CreateRenderer(progressWindow, -1, SDL_RENDERER_ACCELERATED);
+    // if (!progressRenderer)
+    // {
+    //     SDL_DestroyWindow(progressWindow);
+    //     SDL_Log("Failed to create progress renderer: %s", SDL_GetError());
+    //     return;
+    // }
+
+    // int running = 1;
+    // SDL_Event e;
+    // while (running)
+    // {
+    //     while (SDL_PollEvent(&e))
+    //     {
+    //         if (e.type == SDL_QUIT)
+    //         {
+    //             running = 0; // just quit this window's loop
+    //         }
+    //     }
+
+    //     SDL_SetRenderDrawColor(progressRenderer, 200, 200, 255, 255);
+    //     SDL_RenderClear(progressRenderer);
+    //     SDL_RenderPresent(progressRenderer);
+    // }
+
+    // SDL_DestroyRenderer(progressRenderer);
+    // SDL_DestroyWindow(progressWindow);
+    printf("Progress\n");
+    // ⚠️ DO NOT call SDL_Quit() or TTF_Quit() here!
 }
 
 // SDL_Rect boardToScreen(int x, int y)
@@ -343,6 +425,21 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    if (TTF_Init() != 0)
+    {
+        SDL_Log("TTF_Init Error: %s", TTF_GetError());
+        return 1;
+    }
+    TTF_Font *largeFont = TTF_OpenFont("./assets/CutePixel.ttf", 70);  // Large text
+    TTF_Font *mediumFont = TTF_OpenFont("./assets/CutePixel.ttf", 39); // Medium text
+    TTF_Font *smallFont = TTF_OpenFont("./assets/CutePixel.ttf", 28);  // Small text
+
+    if (!largeFont || !mediumFont || !smallFont)
+    {
+        SDL_Log("Failed to load font: %s", TTF_GetError());
+        return 1;
+    }
+
     // 2. Create a window
     SDL_Window *window = SDL_CreateWindow(
         "Eșec",                 // Window title
@@ -374,6 +471,7 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    //
     SDL_Texture *blackKnight = IMG_LoadTexture(renderer, "./assets/knight1.png");
     SDL_Texture *blackPawn = IMG_LoadTexture(renderer, "./assets/pawn1.png");
     SDL_Texture *blackBishop = IMG_LoadTexture(renderer, "./assets/bishop1.png");
@@ -387,6 +485,78 @@ int main(int argc, char *argv[])
     SDL_Texture *whiteRook = IMG_LoadTexture(renderer, "./assets/rook.png");
     SDL_Texture *whiteQueen = IMG_LoadTexture(renderer, "./assets/queen.png");
     SDL_Texture *whiteKing = IMG_LoadTexture(renderer, "./assets/king.png");
+
+    SDL_Color White = {255, 255, 255, 255};
+    SDL_Color Grey = {0, 0, 0, 100};
+    SDL_Color Pink = {240, 209, 250, 255}; // bright pink, outilnes text from menu
+    SDL_Color DarkPink = {185, 50, 230, 255};
+
+    SDL_Surface *textSurface5 = TTF_RenderText_Blended(largeFont, "PixelChess", Grey);
+    SDL_Texture *textTexture5 = SDL_CreateTextureFromSurface(renderer, textSurface5);
+    SDL_Rect textRect5 = {52, 17, textSurface5->w, textSurface5->h};
+    SDL_FreeSurface(textSurface5); // shadow for title
+
+    SDL_Surface *textSurface1 = TTF_RenderText_Blended(largeFont, "PixelChess", White);
+    SDL_Texture *textTexture1 = SDL_CreateTextureFromSurface(renderer, textSurface1);
+    SDL_Rect textRect1 = {60, 10, textSurface1->w, textSurface1->h};
+    SDL_FreeSurface(textSurface1);
+
+    SDL_Surface *textSurface2 = TTF_RenderText_Blended(smallFont, " a    b    c    d     e    f    g    h", White);
+    SDL_Texture *textTexture2 = SDL_CreateTextureFromSurface(renderer, textSurface2);
+    SDL_Rect textRect2 = {92, 480, textSurface2->w, textSurface2->h};
+    SDL_FreeSurface(textSurface2);
+
+    SDL_Surface *textSurface3 = TTF_RenderText_Blended(largeFont, "Menu", Grey);
+    SDL_Texture *textTexture3 = SDL_CreateTextureFromSurface(renderer, textSurface3);
+    SDL_Rect textRect3 = {548, 37, textSurface3->w, textSurface3->h};
+    SDL_FreeSurface(textSurface3); // shadow for menu
+
+    SDL_Surface *textSurface4 = TTF_RenderText_Blended(largeFont, "Menu", White);
+    SDL_Texture *textTexture4 = SDL_CreateTextureFromSurface(renderer, textSurface4);
+    SDL_Rect textRect4 = {555, 30, textSurface4->w, textSurface4->h};
+    SDL_FreeSurface(textSurface4);
+
+    SDL_Surface *textSurfaceStart = TTF_RenderText_Blended(mediumFont, "Start", DarkPink);
+    SDL_Texture *textTextureStart = SDL_CreateTextureFromSurface(renderer, textSurfaceStart);
+    SDL_Rect textRectStart = {572, 130, textSurfaceStart->w, textSurfaceStart->h};
+    SDL_FreeSurface(textSurfaceStart);
+
+    SDL_Surface *textSurfaceSave = TTF_RenderText_Blended(mediumFont, "Save", DarkPink);
+    SDL_Texture *textTextureSave = SDL_CreateTextureFromSurface(renderer, textSurfaceSave);
+    SDL_Rect textRectSave = {572, 210, textSurfaceSave->w, textSurfaceSave->h};
+    SDL_FreeSurface(textSurfaceSave);
+
+    SDL_Surface *textSurfaceReload = TTF_RenderText_Blended(mediumFont, "Reload", DarkPink);
+    SDL_Texture *textTextureReload = SDL_CreateTextureFromSurface(renderer, textSurfaceReload);
+    SDL_Rect textRectReload = {563, 290, textSurfaceReload->w, textSurfaceReload->h};
+    SDL_FreeSurface(textSurfaceReload);
+
+    SDL_Surface *textSurfaceProgress = TTF_RenderText_Blended(mediumFont, "Progress", DarkPink);
+    SDL_Texture *textTextureProgress = SDL_CreateTextureFromSurface(renderer, textSurfaceProgress);
+    SDL_Rect textRectProgress = {546, 370, textSurfaceProgress->w, textSurfaceProgress->h};
+    SDL_FreeSurface(textSurfaceProgress);
+
+    SDL_Surface *textSurfaceStart1 = TTF_RenderText_Blended(mediumFont, "Start", Pink);
+    SDL_Texture *textTextureStart1 = SDL_CreateTextureFromSurface(renderer, textSurfaceStart1);
+    SDL_Rect textRectStart1 = {577, 128, textSurfaceStart1->w, textSurfaceStart1->h};
+    SDL_FreeSurface(textSurfaceStart1);
+
+    SDL_Surface *textSurfaceSave1 = TTF_RenderText_Blended(mediumFont, "Save", Pink);
+    SDL_Texture *textTextureSave1 = SDL_CreateTextureFromSurface(renderer, textSurfaceSave1);
+    SDL_Rect textRectSave1 = {577, 208, textSurfaceSave1->w, textSurfaceSave1->h};
+    SDL_FreeSurface(textSurfaceSave1);
+
+    SDL_Surface *textSurfaceReload1 = TTF_RenderText_Blended(mediumFont, "Reload", Pink);
+    SDL_Texture *textTextureReload1 = SDL_CreateTextureFromSurface(renderer, textSurfaceReload1);
+    SDL_Rect textRectReload1 = {568, 288, textSurfaceReload1->w, textSurfaceReload1->h};
+    SDL_FreeSurface(textSurfaceReload1);
+
+    SDL_Surface *textSurfaceProgress1 = TTF_RenderText_Blended(mediumFont, "Progress", Pink);
+    SDL_Texture *textTextureProgress1 = SDL_CreateTextureFromSurface(renderer, textSurfaceProgress1);
+    SDL_Rect textRectProgress1 = {551, 368, textSurfaceProgress1->w, textSurfaceProgress1->h};
+    SDL_FreeSurface(textSurfaceProgress1);
+
+    const char *multilineText = "1\n2\n3\n4\n5\n6\n7\n8";
 
     if (!blackKnight)
     {
@@ -439,6 +609,7 @@ int main(int argc, char *argv[])
     // 4. Main loop
     int running = 1;
     int move = 0;
+    int started = 0;
     while (running)
     {
 
@@ -452,21 +623,49 @@ int main(int argc, char *argv[])
             else if (event.type == SDL_MOUSEBUTTONDOWN)
             {
                 Coord_t Aux = handleMouseClick(&event.button);
-                if (move % 2 == 0)
+                if (Aux.x > 7 || Aux.x < 0 || Aux.y > 7 || Aux.y < 0)
                 {
-                    move++;
-                    From = Aux;
-                }
-                else
-                {
-                    move++;
-                    To = Aux;
-                    makeMove(From, To);
-                    printf("%d %d : %d %d\n", From.x, From.y, Aux.x, Aux.y);
-                }
+                    int mx = event.button.x;
+                    int my = event.button.y;
 
-                // printf("%d", move);
-                whosTurn = move % 2;
+                    if (mx >= textRectStart.x && mx <= textRectStart.x + textRectStart.w &&
+                        my >= textRectStart.y && my <= textRectStart.y + textRectStart.h)
+                    {
+                        start_game();
+                        started = 1;
+                    }
+                    else if (mx >= textRectSave.x && mx <= textRectSave.x + textRectSave.w && my >= textRectSave.y && my <= textRectSave.y + textRectSave.h)
+                    {
+                        save_game();
+                    }
+                    else if (mx >= textRectReload.x && mx <= textRectReload.x + textRectReload.w && my >= textRectReload.y && my <= textRectReload.y + textRectReload.h)
+                    {
+                        reload_game();
+                    }
+                    else if (mx >= textRectProgress.x && mx <= textRectProgress.x + textRectProgress.w &&
+                             my >= textRectProgress.y && my <= textRectProgress.y + textRectProgress.h)
+                    {
+                        open_progress_window();
+                    }
+                }
+                else if (started == 1)
+                {
+                    if (move % 2 == 0)
+                    {
+                        move++;
+                        From = Aux;
+                    }
+                    else
+                    {
+                        move++;
+                        To = Aux;
+                        makeMove(From, To);
+                        printf("%d %d : %d %d\n", From.x, From.y, Aux.x, Aux.y);
+                    }
+
+                    // printf("%d", move);
+                    whosTurn = move % 2;
+                }
             }
         }
 
@@ -474,24 +673,58 @@ int main(int argc, char *argv[])
         SDL_SetRenderDrawColor(renderer, 48, 0, 72, 1);
         SDL_RenderClear(renderer);
 
+        // the offset
+        SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND); // for transparency
+        SDL_Rect extraRect = {50, 100, 410, 410};                  // x, y, width, height
+        SDL_SetRenderDrawColor(renderer, 176, 75, 165, 150);
+        SDL_RenderFillRect(renderer, &extraRect);
+
         for (int row = 0; row < BOARD_SIZE; row++)
         {
             for (int col = 0; col < BOARD_SIZE; col++)
             {
-                SDL_Rect square = {col * SQUARE_SIZE + 100, row * SQUARE_SIZE + 100, SQUARE_SIZE, SQUARE_SIZE};
+                SDL_Rect square = {col * SQUARE_SIZE + 80, row * SQUARE_SIZE + 80, SQUARE_SIZE, SQUARE_SIZE};
 
                 // Alternate colors
                 if ((row + col) % 2 == 0)
                 {
-                    SDL_SetRenderDrawColor(renderer, 240, 209, 250, 0.8); // White
+                    SDL_SetRenderDrawColor(renderer, 240, 209, 250, 255); // White
                 }
                 else
                 {
-                    SDL_SetRenderDrawColor(renderer, 185, 50, 230, 0.8); // Dark gray
+                    SDL_SetRenderDrawColor(renderer, 185, 50, 230, 255); // for black //dark purple
                 }
                 SDL_RenderFillRect(renderer, &square);
             }
         }
+
+        SDL_RenderCopy(renderer, textTexture5, NULL, &textRect5); // CONTEAZA ORDINEA
+        SDL_RenderCopy(renderer, textTexture1, NULL, &textRect1);
+        SDL_RenderCopy(renderer, textTexture2, NULL, &textRect2);
+        SDL_RenderCopy(renderer, textTexture3, NULL, &textRect3);
+        SDL_RenderCopy(renderer, textTexture4, NULL, &textRect4);
+        SDL_RenderCopy(renderer, textTextureStart, NULL, &textRectStart);
+        SDL_RenderCopy(renderer, textTextureSave, NULL, &textRectSave);
+        SDL_RenderCopy(renderer, textTextureReload, NULL, &textRectReload);
+        SDL_RenderCopy(renderer, textTextureProgress, NULL, &textRectProgress);
+        SDL_RenderCopy(renderer, textTextureStart1, NULL, &textRectStart1);
+        SDL_RenderCopy(renderer, textTextureSave1, NULL, &textRectSave1);
+        SDL_RenderCopy(renderer, textTextureReload1, NULL, &textRectReload1);
+        SDL_RenderCopy(renderer, textTextureProgress1, NULL, &textRectProgress1);
+        render_multiline_text(renderer, smallFont, multilineText, White, 63, 107, 18);
+
+        SDL_Rect outline1 = {534, 120, 160, 60};
+        SDL_SetRenderDrawColor(renderer, 240, 209, 250, 255); // Green
+        SDL_RenderDrawRect(renderer, &outline1);
+        SDL_Rect outline2 = {534, 200, 160, 60};
+        SDL_SetRenderDrawColor(renderer, 240, 209, 250, 255); // Green
+        SDL_RenderDrawRect(renderer, &outline2);
+        SDL_Rect outline3 = {534, 280, 160, 60};
+        SDL_SetRenderDrawColor(renderer, 240, 209, 250, 255); // Green
+        SDL_RenderDrawRect(renderer, &outline3);
+        SDL_Rect outline4 = {534, 360, 160, 60};
+        SDL_SetRenderDrawColor(renderer, 240, 209, 250, 255); // Green
+        SDL_RenderDrawRect(renderer, &outline4);
 
         showChessTable();
 
